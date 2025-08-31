1d1c67b3d9905331c3a28c1fb4a6fb52d82bb6d7
acroofing2020-del <acroofing2020@gmail.com>
2025-08-31 23:43:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 10000,
+    host: "0.0.0.0",
+    allowedHosts: ["ace-roofing.onrender.com"]  // 👈 add your Render URL here
+  },
   server: {
     host: "0.0.0.0",
     port: 10000
